bf5dbc8af036a6aad51eaba191026d6698a2b63d
Mousey <tom@deadlyllama.com>
2019-09-17 20:17:35 +0100

    Add comment to method

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+* This function will run when the page loads, once the browser
+* has detected we're ready to go it'll log "hello world to the console"
+*/
 (function() {
     console.log('hello world');
 })
